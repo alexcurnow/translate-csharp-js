9c85b85291631c021b934670fe7a07737c2566b5
Alex Curnow <alexcurnow@gmail.com>
2020-05-22 14:33:32 -0500

    completed place names

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,6 +1,18 @@
 function main() {
-  // Put your code here
+  const names = ["Nashville", "Hong Kong", "The back yard", "Earth",
+    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"]
 
+  console.log("All Place Names")
+
+  for (let name of names) {
+    console.log(name)
+  }
+
+  const theNames = names.filter(n => n.startsWith("The"))
+  console.log("'The' Place Names")
+  for (let theName of theNames) {
+    console.log(theName)
+  }
 }
 
 main();
