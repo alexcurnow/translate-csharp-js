ae7d3f350a5289e3fb258cdeb06d7c9f85c2a1d7
Alex Curnow <alexcurnow@gmail.com>
2020-05-22 14:52:13 -0500

    Completed enemies-list

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -1,5 +1,60 @@
 function main() {
-  // Put your code here
+  console.log("My Enemies List!")
+  console.log("---------------")
+
+  const getEnemies = () => {
+    let enemies = [
+      {
+        firstName: "Joshua",
+        lastName: "Flowers",
+        offenses: [
+          "Being a jerk to me in elementary school",
+          "Not being nice to me in elementary school"
+        ],
+        isReallyHated: true
+      },
+      {
+        firstName: "Darth",
+        lastName: "Vader",
+        offenses: [
+          "Cut off Luke's hand",
+          "Murdered all those kids",
+          "Unkind management practices"
+        ],
+        isReallyHated: false
+      },
+      {
+        firstName: "Betty",
+        lastName: "Rudelady",
+        offenses: [
+          "Phone calls in the theater",
+          "Phone calls on the bus",
+          "Phone calls in line at the grocery store",
+          "Poor conversationalist"
+        ],
+        isReallyHated: true
+      },
+      {
+        firstName: "Leon",
+        lastName: "Peck",
+        offenses: [
+          "Keeps giving me a hotplate"
+        ],
+        isReallyHated: false
+      }
+    ]
+    return enemies
+  }
+
+  const enemiesList = getEnemies()
+
+  enemiesList.forEach(e => {
+    if (e.isReallyHated) {
+      console.log(`${e.firstName} ${e.lastName} (Really, really dislike!)`)
+    } else {
+      console.log(`${e.firstName} ${e.lastName}`)
+    }
+  })
 
 }
 
